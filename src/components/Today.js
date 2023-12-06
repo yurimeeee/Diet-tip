c806f24510ab75eb11fc929313c0aa2af6123a89
triallife <imlinkseo@gmail.com>
2023-12-06 20:51:04 +0900

    [ADD] board

--- src/components/Today.js
+++ src/components/Today.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import badSticker from '../asset/bad-sticker.png';
 import goodsticker from '../asset/good-sticker.png';
 import { useLocation } from 'react-router-dom';
+import Weekly from "./Weekly";
 
 const Today = () => {
   const location = useLocation();
@@ -38,34 +39,38 @@ const Today = () => {
 
 
   return(
-    <main className="Today">
+    <div className="Today">
       <div className='today'>
         <h5>오늘의</h5>
-        <div className='today-cards'>
-          {mainYN ? 
-              <div className='today-card-big lg-radius'>
-                  <h6>날씨</h6>
-                  <div className="weather">
-                    <div className="weather-text">
-                      <p>최저<span>{lt}</span></p>
-                      <p>최고<span>{ht}</span></p>
+        <div className="today_content">
+          {mainYN ? <Weekly/> : ''}
+          <div className='today-cards'>
+            {mainYN ? 
+                <div className='today-card-big lg-radius'>
+                    <h6>날씨</h6>
+                    <div className="weather">
+                      <div className="weather-text">
+                        <p>최저<span>{lt}</span></p>
+                        <p>최고<span>{ht}</span></p>
+                      </div>
+                      <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt='today weather icon' className='weather-icon'/>
                     </div>
-                    <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt='today weather icon' className='weather-icon'/>
-                  </div>
-              </div>
-            : 
-            ''}
-          <div className='today-card lg-radius'>
-            <h6>식단</h6>
-            <img src={badSticker} alt='today meal sticker' className='sticker'/>
-          </div>
-          <div className='today-card lg-radius'>
-            <h6>운동</h6>
-            <img src={goodsticker} alt='today meal sticker' className='sticker'/>
+                </div>
+              : 
+              ''}
+            <div className='today-card lg-radius'>
+              <h6>식단</h6>
+              <img src={badSticker} alt='today meal sticker' className='sticker'/>
+            </div>
+            <div className='today-card lg-radius'>
+              <h6>운동</h6>
+              <img src={goodsticker} alt='today meal sticker' className='sticker'/>
+            </div>
           </div>
         </div>
+        
       </div>
-    </main>
+    </div>
   )
 };
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,31 @@
 import React from "react";
 import '../styles/main.css';
 import Banner from "../components/Banner";
-import Weekly from "../components/Weekly";
 import Today from "../components/Today";
+import mealImg from "../asset/meal/meal.png";
 
 const Main = () => {
   return(
-    <div className="Main container">
+    <main className="Main container">
       <Banner/>
       <Today/>
-    </div>
-
+      <div className="hot-board">
+        <h5>HOT한 다이어팁! 인기 게시물</h5>
+        <div className="hot-board-buttons">
+          <button className="w-green-btn" type="button">식단공유</button>
+          <button className="w-green-btn" type="button">추천제품</button>
+          <button className="w-green-btn" type="button">운동인증</button>
+          <button className="w-green-btn" type="button">자유게시판</button>
+          <button className="w-green-btn" type="button">Q&A</button>
+        </div>
+        <div className="hot-board-main">
+          <div className="img-container">
+            <img alt="hot meal" src={mealImg} className="board-img"></img>
+          </div>
+        </div>
+        
+      </div>
+    </main>
   )
 };
 
