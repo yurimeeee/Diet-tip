0d4fe0b9b70583b1a1c77b60ca2ef42dd9831645
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-12-11 00:28:11 +0900

    [ADD] react-select로 카테고리 custom

--- src/pages/FreeBoard.js
+++ src/pages/FreeBoard.js
@@ -1,37 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "react-select"; //react-select 라이브러리
+import styled from "styled-components"; //css-in-js 라이브러리
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil, faMagnifyingGlass, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faImage, faCommentDots, faThumbsUp, faEye } from "@fortawesome/free-regular-svg-icons";
 import "../styles/community.scss";
+import Banner from "../asset/community/banner_freeboard.png";
+
+const SelectBox = styled(Select).attrs({
+  classNamePrefix: 'react-select',
+})`
+  .react-select__control {
+    cursor: pointer;
+    background: transparent;
+    border-radius: 66px;
+    border: 1px solid #32a061;
+    box-shadow: none;
+    // width: 136px;
+    // text-align: center;
+    &:hover{
+      border: 1px solid #32a061;
+    }
+  }
+  .react-select__single-value {
+    color: #32a061; /* 텍스트 색상 지정 */
+    font-size: 16px;
+  }
+  .react-select__menu {
+    background-color: #f2f3f5;
+    border-radius: 10px;
+    border: 1px solid #32a061;
+    box-shadow: none;
+    overflow: hidden;
+  }
+  .react-select__option {
+    background-color: transparent; /* option 배경색 */
+    color: black; /* option 텍스트 색상 */
+  }
+  .react-select__option--is-selected {
+    background-color: #32a061; /* 클릭된 option 배경색 */
+    color: white; /* click 옵션 텍스트 색상 */
+  }
+  .react-select__option--is-focused {
+    background-color: #94dfb6;
+    color: #000; /* hover 옵션 텍스트 색상 */
+  }
+  .react-select__option:active{
+    background-color: #94dfb6;
+  }
+`;
 
 const FreeBoard = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = [
+    { value: 'all', label: '전체' },
+    { value: 'review', label: '후기' },
+    { value: 'beforeAfter', label: '비포&애프터' },
+    { value: 'tip', label: '꿀 TIP' },
+    { value: 'etc', label: '기타' },
+  ];
+
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+  };
+
+  
+
+
   return(
-    
-    <main>
+    <main className="Community">
       <section className="fb-banner bg-point-1 pd">
         <div className="container df jcsb">
           <div className="fb-banner-content">
             <h2 className="tt4 bold white">자유게시판</h2>
             <p className="tt2 bold white mg-t1">경험, 정보, 후기! 무엇이든 자유롭게 공유해보세요!</p>
             <form className="mg-t2 search">
-              <button type="submit"><i className="fa-solid fa-magnifying-glass point-1"></i></button>
+              <button type="submit" className="point-1">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
               <input type="text" placeholder="검색어를 입력해주세요." />
             </form>
           </div>
-          <img src="banner_freeboard.png" alt="" />
+          <img src={Banner} alt="" />
         </div>
       </section>
   
       <div className="container">
-        <div>
-          <div className="free-select mg-t3">
-            {/* <span><!-- 선택된 옵션 출력 --></span> */}
-            <ul className="hidden">
-              <li>전체</li>
-              <li>후기</li>
-              <li>비포&애프터</li>
-              <li>꿀 TIP</li>
-              <li>기타</li>
-            </ul>
+        <div className="df mg-t3">
+          <div className="free-select">
+            <SelectBox
+              value={selectedOption}
+              onChange={handleChange}
+              options={options}
+            />
           </div>
-          <button className="w-green-btn"><i className="fa-solid fa-pencil"></i> 글 쓰기</button>
+
+          
+
+          <button className="w-green-btn">
+            <FontAwesomeIcon icon={faPencil} /> 글 쓰기
+          </button>
         </div>
   
         <table className="mg-t1">
@@ -53,8 +122,12 @@ const FreeBoard = () => {
               <td className="free-td-3 link"><a href="" className="link">평양 마라톤 풀코스 참가 후기</a></td>
               <td className="free-td-4">아임닭맛있닭</td>
               <td className="free-td-5">2023-12-02</td>
-              <td className="free-td-6">136</td>
-              <td className="free-td-7">954</td>
+              <td className="free-td-6">
+                <FontAwesomeIcon icon={faThumbsUp} className="mg-r1 gray-3" />136
+              </td>
+              <td className="free-td-7">
+                <FontAwesomeIcon icon={faEye} className="mg-r1 gray-3" />954
+              </td>
             </tr>
           </tbody>
         </table>
