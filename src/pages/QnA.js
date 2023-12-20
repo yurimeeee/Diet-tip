7bbb92b2fe1f36e525fa0566b53209c9c18967b2
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-12-21 05:29:24 +0900

    [ADD] 페이지네이션 컴포넌트 연결

--- src/pages/QnA.js
+++ src/pages/QnA.js
@@ -10,10 +10,8 @@ import icon_q from "../asset/community/icon_q.png"
 import level_1 from "../asset/level-1-badge.png";
 import level_2 from "../asset/level-2-badge.png";
 import level_3 from "../asset/level-3-badge.png";
-
 import PaginationComp from "../components/Pagination";
 import QnaView from "../components/QnaView";
-
 
 const QnA = () => {
   const levelImg = {
@@ -22,11 +20,11 @@ const QnA = () => {
     '3': level_3,
   };
   
-  const [allData, setAllData] = useState([]);
-  const [topPostsData, setTopPostsData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
+  const [ allData, setAllData ] = useState([]);
+  const [ topPostsData, setTopPostsData ] = useState([]);
+  const [ filteredData, setFilteredData ] = useState([]);
   const itemsPerPage = 15;
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -42,9 +40,10 @@ const QnA = () => {
           .sort((a, b) => b.thumbsUp - a.thumbsUp)
           .slice(0, 5);
         setTopPostsData(topPosts);
+        setFilteredData(boardData.slice(0, itemsPerPage)); //페이지에 15개의 게시글 출력
 
-        //한 페이지에 15개의 게시글 출력
-        setFilteredData(boardData.slice(0, itemsPerPage));
+        setSelectedCate('전체');
+        filterDataByCategory('전체');
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -53,6 +52,7 @@ const QnA = () => {
     fetchData();
   }, []);
 
+  //date 포맷 함수
   const formatDate = (date) => {
     const year = date.getFullYear();
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
@@ -73,25 +73,45 @@ const QnA = () => {
     });
   };
 
+  //카테고리
+  const [ categories, setCategories ] = useState(['전체', '식단', '운동', '기타']);
+  const [ selectedCate, setSelectedCate ] = useState('전체');
+
   const handleCateClick = (category) => {
-    // Set selected category and filter data
     setSelectedCate(category);
     filterDataByCategory(category);
   };
 
+  //카테고리 선택시 전체 데이터에서 필터링 
   const filterDataByCategory = (category) => {
-    // Filter data based on selected category from the original boardData
     const filteredData = category === '전체' ? allData : allData.filter((post) => post.category === category);
-    setFilteredData(filteredData.slice(0, itemsPerPage)); // Apply limit for display
+    setSelectedCateData(filteredData);
+    setCurrentPage(1); // 새로운 카테고리로 전환될 때 현재 페이지를 1로 설정
   };
 
-  const [categories, setCategories] = useState(['전체', '식단', '운동', '기타']);
-  const [selectedCate, setSelectedCate] = useState('');
+  //페이지네이션
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const [ totalPageCount, setTotalPageCount ] = useState(1);
+  const [ currentPageData, setCurrentPageData ] = useState([]);
+  const [ selectedCateData, setSelectedCateData ] = useState([]);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-
+  // allData가 업데이트될 때 페이지네이션을 다시 렌더링
+  useEffect(() => {
+    let dataForPagination = selectedCateData.length > 0 ? selectedCateData : allData;
+    setTotalPageCount(Math.ceil(dataForPagination.length / itemsPerPage));
   
+    if (dataForPagination.length > 0) {
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      setCurrentPageData(dataForPagination.slice(startIndex, endIndex));
+    }
+  }, [selectedCateData, allData, currentPage, itemsPerPage]);
+  
+
   console.log(allData);
-  console.log(filteredData);
 
   return(
     <main className="Community">
@@ -127,8 +147,9 @@ const QnA = () => {
           <div className="top-posts bg-green-2 lg-radius sm-shadow">
             <h3 className="tt5 bold">주간 인기글</h3>
             <div className="posts-box df">
-              {topPostsData.map((post) => (
-                <p key={post.id}>
+              {topPostsData.map((post, index) => (
+                <p key={post.id} className="df">
+                  <span className="posts-number bold">{index + 1}.</span>
                   <a href="" className="link">
                     {post.title}
                     <b> <FontAwesomeIcon icon={faThumbsUp} className="mg-r1 gray-3" />{post.thumbsUp}</b>
@@ -159,7 +180,7 @@ const QnA = () => {
           </thead>
       
           <tbody>
-            {filteredData.map(( item ) => (
+            {currentPageData.map(( item ) => (
               <tr key={item.id}>
                 <td className="qna-td-1"><img src={icon_q} alt="" /></td>
                 <td className="qna-td-2 green-4">{item.category}</td>
@@ -191,17 +212,11 @@ const QnA = () => {
           </tbody>
         </table>
       
-        <nav className="pagination mg-t3">
-          <ul className="pagination">
-            <li className="page-item first-page">맨앞</li>
-            <li className="page-item">1</li>
-            <li className="page-item">2</li>
-            <li className="page-item active">3</li>
-            <li className="page-item">4</li>
-            <li className="page-item">5</li>
-            <li className="page-item last-page">맨뒤</li>
-          </ul>
-        </nav>
+        <PaginationComp
+          currentPage={currentPage}
+          totalPageCount={totalPageCount}
+          handlePageChange={handlePageChange}
+        />
       </div>
     </main>
   );
